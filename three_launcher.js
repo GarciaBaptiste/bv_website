77a22b87a49716d9b2181a4c3fc213f7623fe2a5
GarciaBaptiste <baptiste1garcia@gmail.com>
2020-08-02 12:54:18 +0200

    add moving images plan

--- three_launcher.js
+++ three_launcher.js
@@ -6,10 +6,14 @@ let worldScene = null;
 let renderer = null;
 let camera = null;
 let clock = null;
+let forwardSpeed = 0,
+  straffeSpeed = 0;
 
 let MODELS = [{ name: "room" }];
 
 let numLoadedModels = 0;
+
+let imagesContainer;
 
 function setup() {
   initScene();
@@ -41,15 +45,14 @@ function loadGltfModel(model, onLoaded) {
 
       gltf.scene.traverse(function (object) {
         if (object.isMesh) {
-          console.log(object);
           object.castShadow = true;
           object.receiveShadow = true;
         } else if (object.isLight) {
           object.castShadow = true;
-          object.shadow.mapSize.width = 1024;
-          object.shadow.mapSize.height = 1024;
+          object.shadow.mapSize.width = 2048;
+          object.shadow.mapSize.height = 2048;
           object.shadow.camera.near = 0.1;
-          object.shadow.camera.far = 500;
+          object.shadow.camera.far = 50;
           object.shadow.bias = -0.005;
         }
       });
@@ -64,7 +67,6 @@ function loadGltfModel(model, onLoaded) {
 
 function animate() {
   requestAnimationFrame(animate);
-  moveCamera();
   renderer.render(worldScene, camera);
 }
 
@@ -87,36 +89,49 @@ function initScene() {
     5000
   );
   camera.rotation.order = "YXZ";
-  camera.position.x = -1.5;
-  camera.position.y = 1.7;
-  camera.position.z = 1.5;
-  camera.rotation.y = 0.2;
-  camera.rotation.x = 0.2;
+  camera.position.x = 16;
+  camera.position.y = 5;
+  camera.position.z = 16;
+  camera.rotation.x = -0.0925;
+  camera.rotation.y = 0.59;
 
   clock = new THREE.Clock();
 
   worldScene = new THREE.Scene();
-  worldScene.background = new THREE.Color(0x000000);
-  // worldScene.fog = new THREE.Fog(0x000000, 10, 30);
+  worldScene.background = new THREE.Color(0xffffff);
+  worldScene.fog = new THREE.Fog(0xffffff, 10, 90);
 
-  const hlight = new THREE.AmbientLight(0xffffff, 0.05);
+  const hlight = new THREE.AmbientLight(0xffffff, 0.75);
   worldScene.add(hlight);
 
-  let light = new THREE.PointLight(0xffffff, 0.5, 50, 15);
-  light.position.set(0, 3, 0);
+  let light = new THREE.PointLight(0xffffff, 1.5, 550, 25);
+  light.position.set(17, 9, 50);
   worldScene.add(light);
   light.castShadow = true;
   light.shadow.mapSize.width = 2048;
   light.shadow.mapSize.height = 2048;
-  light.shadow.camera.near = 1;
-  light.shadow.camera.far = 1000;
+  light.shadow.camera.near = 10;
+  light.shadow.camera.far = 100;
   light.shadow.bias = -0.005;
-  light.shadow.radius = 1;
+  light.shadow.radius = 10;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function setupScene() {
+  imagesContainer = document.getElementById("images-plan-selector");
+  let images = document.getElementById("images-plan").children;
+  console.log(images);
+  for (var i = 0; i < images.length; i++) {
+    let valueX = Math.random() * 100;
+    let valueY = Math.random() * 100;
+    images[i].style.left = "calc(" + valueX + "% - " + valueX / 5 + "vw)";
+    images[i].style.top = "calc(" + valueY + "% - " + valueY / 5 + "vw)";
+  }
+
+  const mesh = MODELS[0].scene;
+  worldScene.add(mesh);
+
   animate();
 }
 
@@ -128,4 +143,15 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function mouseMoved(evt) {}
+function mouseMoved(evt) {
+  let ratioX = evt.clientX / window.innerWidth;
+  camera.position.x = (ratioX / 3) * 24 + 4;
+  camera.position.z = (1 - ratioX / 3) * 16 + 8;
+  camera.rotation.x = -((ratioX / 3.6) * 0.025 + 0.08);
+  camera.rotation.y = (ratioX / 3.6) * 0.94 + 0.12;
+  let ratioY = evt.clientY / window.innerHeight;
+  camera.position.y = 7 - ratioY;
+
+  imagesContainer.style.left = (1 - ratioX) * 100 + "vw";
+  imagesContainer.style.top = (1 - ratioY) * 100 + "vh";
+}
