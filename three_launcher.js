9d78f1c1605119c9f861fc379998a3d2402f796d
Baptiste Garcia <baptiste1garcia@gmail.com>
2020-09-12 13:45:48 +0200

    WIP set relative values for scroll and animation

--- three_launcher.js
+++ three_launcher.js
@@ -7,6 +7,7 @@ let camera = null;
 let clock = null;
 let mixer = null;
 let gltf = null;
+let content = null;
 
 let MODELS = [{ name: "test_camera_anim" }];
 
@@ -14,13 +15,14 @@ let numLoadedModels = 0;
 
 function setup() {
   document.getElementById("content-page").addEventListener("scroll", scrolled);
+  content = document.getElementById('content');
   loadModels();
 }
 
 function loadModels() {
   for (let i = 0; i < MODELS.length; ++i) {
     const m = MODELS[i];
-    loadGltfModel(m, function () {
+    loadGltfModel(m, function() {
       ++numLoadedModels;
       if (numLoadedModels === MODELS.length) {
         document.getElementById("loader").style.display = "none";
@@ -36,13 +38,13 @@ function loadGltfModel(model, onLoaded) {
   const modelName = "models/" + model.name + ".gltf";
   loader.load(
     modelName,
-    function (gltf_response) {
+    function(gltf_response) {
       gltf = gltf_response;
       let scene = gltf.scene;
       camera = gltf.cameras[0];
       model.scene = scene;
 
-      gltf.scene.traverse(function (object) {
+      gltf.scene.traverse(function(object) {
         if (object.isMesh) {
           object.castShadow = true;
           object.receiveShadow = true;
@@ -58,7 +60,7 @@ function loadGltfModel(model, onLoaded) {
       });
       onLoaded();
     },
-    function (xhr) {
+    function(xhr) {
       document.getElementById("loader").innerText =
         "Loading model " + Math.round((xhr.loaded / xhr.total) * 100) + "%";
     }
@@ -125,12 +127,23 @@ function render() {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function onWindowResize() {
+function setSizeCamera() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+function contentSize() {
+  return content.offsetHeight - window.innerHeight;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function onWindowResize() {
+  setSizeCamera();
+}
+
 function scrolled(evt) {
+  console.log(evt.target.scrollTop);
   mixer.setTime((evt.target.scrollTop / 4000) * 8.5);
 }
