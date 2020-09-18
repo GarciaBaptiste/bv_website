4d9bc7c8552fa925ad2c2752ef348b1d2f04cd3d
GarciaBaptiste <baptiste1garcia@gmail.com>
2020-09-18 22:15:14 +0200

    feature automatic graphic settings regarding to framerate

--- three_launcher.js
+++ three_launcher.js
@@ -15,6 +15,7 @@ let content = null,
   contentHeight = 0;
 let animLength = null;
 let previousScroll = 0;
+let mesh = null;
 
 let MODELS = [{ name: "test_camera_anim2.glb" }];
 
@@ -58,23 +59,19 @@ function loadGltfModel(model, onLoaded) {
       camera.fov = 40;
       camera.far = 120;
       model.scene = scene;
-
       gltf.scene.traverse(function (object) {
         if (object.isMesh) {
           object.castShadow = true;
           object.receiveShadow = true;
-          object.matrixAutoUpdate = false;
-          object.updateMatrix();
+          object.matrixAutoUpdate = true;
         } else if (object.isLight) {
           object.castShadow = true;
-          // object.shadow.mapSize.width = 32;
-          // object.shadow.mapSize.height = 32;
+          object.shadow.mapSize.width = 64;
+          object.shadow.mapSize.height = 64;
           object.shadow.camera.near = 0.1;
           object.shadow.camera.far = 50;
-          object.shadow.bias = -0.005;
-          object.shadow.radius = 1;
-          object.matrixAutoUpdate = false;
-          object.updateMatrix();
+          object.shadow.bias = -0.015;
+          object.matrixAutoUpdate = true;
         }
       });
       onLoaded();
@@ -89,11 +86,11 @@ function loadGltfModel(model, onLoaded) {
 function initRenderer() {
   let container = document.getElementById("context");
   renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setPixelRatio(1);
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.outputEncoding = THREE.sRGBEncoding;
   renderer.shadowMap.enabled = true;
-  renderer.shadowMap.type = THREE.BasicShadowMap;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
   container.appendChild(renderer.domElement);
 }
 
@@ -105,12 +102,12 @@ function initScene() {
   setSizeCamera();
   worldScene = new THREE.Scene();
   worldScene.background = new THREE.Color(0xffffff);
-  // worldScene.fog = new THREE.Fog(0xffffff, 10, 120);
+  worldScene.fog = new THREE.Fog(0xffffff, 5, 100);
 
-  const hlight = new THREE.AmbientLight(0xffffff, 0.85);
+  const hlight = new THREE.AmbientLight(0xffffff, 0.5);
   worldScene.add(hlight);
 
-  const mesh = MODELS[0].scene;
+  mesh = MODELS[0].scene;
   worldScene.add(mesh);
 
   mixer = new THREE.AnimationMixer(camera);
@@ -126,7 +123,22 @@ function animate() {
 let goingForward = true;
 let delta = 0.02;
 
+let lastLoop = new Date();
+
+let fps = 1;
+
+window.setInterval(function () {
+  if (fps < 25) {
+    testLowerSettings();
+  } else if (fps > 58) {
+    testHigherSettings();
+  }
+}, 3000);
+
 function render() {
+  const thisLoop = new Date();
+  fps = 1000 / (thisLoop - lastLoop);
+  lastLoop = thisLoop;
   // const roundedTime = Math.round(mixer.time * 10) / 10;
   if (mixer) {
     // if (goingForward) {
@@ -164,6 +176,56 @@ function getContentHeight() {
 
 function scrollRatio() {
   return contentPage.scrollTop / contentHeight;
+}
+
+let graphicSettingsIndex = 3;
+
+const graphicSettings = [
+  { shadowMap: false, mapSize: 16, fog: false, bias: -0.05 },
+  { shadowMap: false, mapSize: 16, fog: true, bias: -0.05 },
+  { shadowMap: true, mapSize: 16, fog: true, bias: -0.05 },
+  { shadowMap: true, mapSize: 32, fog: true, bias: -0.05 },
+  { shadowMap: true, mapSize: 64, fog: true, bias: -0.015 },
+  { shadowMap: true, mapSize: 128, fog: true, bias: -0.01 },
+  { shadowMap: true, mapSize: 256, fog: true, bias: -0.007 },
+  { shadowMap: true, mapSize: 512, fog: true, bias: -0.005 },
+  { shadowMap: true, mapSize: 1024, fog: true, bias: -0.003 },
+];
+
+function testLowerSettings() {
+  if (graphicSettingsIndex > 0) {
+    graphicSettingsIndex--;
+    resetGraphicSettings();
+  }
+}
+
+function testHigherSettings() {
+  if (graphicSettingsIndex < graphicSettings.length - 1) {
+    graphicSettingsIndex++;
+    resetGraphicSettings();
+  }
+}
+
+function resetGraphicSettings() {
+  const elemsToTest = mesh.children;
+  for (let i = 0; i < elemsToTest.length; i++) {
+    if (elemsToTest[i].type === "PointLight") {
+      elemsToTest[i].castShadow =
+        graphicSettings[graphicSettingsIndex].shadowMap;
+      elemsToTest[i].shadow.mapSize.width =
+        graphicSettings[graphicSettingsIndex].mapSize;
+      elemsToTest[i].shadow.mapSize.height =
+        graphicSettings[graphicSettingsIndex].mapSize;
+      elemsToTest[i].shadow.bias = graphicSettings[graphicSettingsIndex].bias;
+      elemsToTest[i].shadow.map.dispose();
+      elemsToTest[i].shadow.map = null;
+    } else if (elemsToTest[i].type === "Mesh") {
+      elemsToTest[i].castShadow =
+        graphicSettings[graphicSettingsIndex].shadowMap;
+      elemsToTest[i].receiveShadow =
+        graphicSettings[graphicSettingsIndex].shadowMap;
+    }
+  }
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
